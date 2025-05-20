f23ddb0ff5e84763ee02eeb52ebdbcfb64c8825d
Kris9000 <Kris9000@users.noreply.github.com>
2025-05-20 15:16:07 +0000

    deploy: cae7eb7c5fcb2ff657eb2c8e70b745937b3fe15f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3853329834"
+    serviceWorkerVersion: "4085326419"
   }
 });
