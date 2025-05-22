608bca57fa148c53c1f9316d4fa306aa65e7464b
Kris <kris.boonefaes@gmail.com>
2025-05-22 12:19:35 +0200

    notif bubble

--- lib/molecules/molecules.dart
+++ lib/molecules/molecules.dart
@@ -7,6 +7,7 @@ export 'flow/static_flow_widget.dart';
 export 'licenseplate/license_plate.dart';
 export 'licenseplate/license_plate_size.dart';
 export 'map_markers/map_marker.dart';
+export 'notification_bubble/notification_bubble.dart';
 export 'chips/chips.dart';
 export 'properties/property_widget.dart';
 export 'properties/price_widget.dart';
